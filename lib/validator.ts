4a75621c939d60b84aedd01ae7a5d93ba215addd
rhsyadav <y.kaushal161183@gmail.com>
2025-03-18 09:55:14 +0000

    Session 28 - Place order - Model Migration Schema and Types

--- lib/validator.ts
+++ lib/validator.ts
@@ -71,3 +71,25 @@ export const paymentMethodSchema = z.object({
     path: ['type'],
     message: 'Invalid payment method'
 });
+
+
+export const insertOrderSchema = z.object({
+    userId: z.string().min(1, 'User is required'),
+    itemsPrice: currency,
+    totalPrice: currency,
+    shippingPrice: currency,
+    taxPrice: currency,
+    paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+        message: 'Invalid payment method'
+    }),
+    shippingAddress: shippingAddressSchema
+});
+
+export const insertOrderItemSchema = z.object({
+    productId: z.string(),
+    slug: z.string(),
+    image: z.string(),
+    name: z.string(),
+    price: currency,
+    qty: z.number(),
+});
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,4 @@
-import { cartItemSchema, insertCartSchema, insertProductSchema, paymentMethodSchema, shippingAddressSchema, signUpFormSchema } from '@/lib/validator';
+import { cartItemSchema, insertCartSchema, insertOrderItemSchema, insertOrderSchema, insertProductSchema, paymentMethodSchema, shippingAddressSchema, signUpFormSchema } from '@/lib/validator';
 import { z } from 'zod';
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -16,3 +16,18 @@ export type CartItem = z.infer<typeof cartItemSchema>
 // Type for Shipping Address
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>
 export type PaymentMethod = z.infer<typeof paymentMethodSchema>
+
+// Type for Order Item
+export type OrderItem = z.infer<typeof insertOrderItemSchema>
+// Type for Order
+export type Order = z.infer<typeof insertOrderSchema> & {
+    id: string;
+    createdAt: Date;
+    isPaid: boolean;
+    paidAt: Date | null;
+    isDelivered: boolean;
+    deliveredAt: Date | null;
+    orderitems: OrderItem[];
+    user: { name: string; email: string };
+}
+
